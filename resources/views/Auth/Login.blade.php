5ea9cf29bd5f5a0bcaa7cc2736d30edf0b21e1f6
Javier Hevia <javierhc22@gmail.com>
2018-03-08 23:09:19 -0600

    ya verifico

--- resources/views/Auth/Login.blade.php
+++ resources/views/Auth/Login.blade.php
@@ -6,14 +6,10 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-<<<<<<< HEAD
-        <title>LogIn</title>
-=======
         <title>LOGIN</title>
->>>>>>> master
 
         <!-- CSS -->
-
+        <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:400,100,300,500">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
 		    <link rel="stylesheet" href="css/form-elements.css">
@@ -37,28 +33,6 @@
 
     <body>
 
-          <!-- Navigation -->
-          <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-            <div class="container">
-              <a class="navbar-brand" href="/">Call Center E-Sports</a>
-              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ml-auto">
-                  <li class="nav-item active">
-                    <a class="nav-link" href="/">Home
-                      <span class="sr-only">(current)</span>
-                    </a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link " href="/Register"><i class="fa fa-sign-up"></i> Sign Up</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
         <!-- Top content -->
         <div class="top-content">
 
@@ -66,36 +40,16 @@
                 <div class="container">
                     <div class="row">
                         <div class="col-sm-8 col-sm-offset-2 text">
-<<<<<<< HEAD
-                            <h1><strong>Call Center E-Sports</strong></h1>
-                            <div class="description">
-                            	<p>
-                              Ingrese sus datos.
-                             </p>
-                            </div>
-=======
                             <h1><strong>Call-Center E-Sports</strong> Login </h1>
                            
->>>>>>> master
                         </div>
                     </div>
-
                     <div class="row">
                         <div class="col-sm-6 col-sm-offset-3 form-box">
                         	<div class="form-top">
                         		<div class="form-top-left">
-<<<<<<< HEAD
-                        			<h3>Login to our site</h3>
-                            		<p>Enter your username and password to log on:</p>
-                                <h3>No tienes cuenta Registrate</h3>
-
-                                  <a class="btn btn-link-2" href="/Register">
-                                     Registrarse
-                                  </a>
-=======
                         			<h3>LOGIN</h3>
                             		<p>Intruduzca su nombre de usuario y su contraseñia :D</p>
->>>>>>> master
                         		</div>
                         		<div class="form-top-right">
                         			<i class="fa fa-lock"></i>
@@ -116,17 +70,26 @@
 		                    </div>
                         </div>
                     </div>
-
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 social-login">
+                        	<h3>...or login with:</h3>
+                        	<div class="social-login-buttons">
+	                        	<a class="btn btn-link-2" href="#">
+	                        		<i class="fa fa-facebook"></i> Facebook
+	                        	</a>
+	                        	<a class="btn btn-link-2" href="#">
+	                        		<i class="fa fa-twitter"></i> Twitter
+	                        	</a>
+	                        	<a class="btn btn-link-2" href="#">
+	                        		<i class="fa fa-google-plus"></i> Google Plus
+	                        	</a>
+                        	</div>
+                        </div>
+                    </div>
                 </div>
-
-
             </div>
 
         </div>
-
-
-
-
 
 
         <!-- Javascript -->
